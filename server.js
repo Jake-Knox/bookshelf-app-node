0bba491af6a5c84da1bb29f3bf10b2123049303b
jake-knox- <jakeknox11@hotmail.co.uk>
2023-07-10 20:04:04 +0100

    hardcoded method of adding to database

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 
 // Database setup
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
 const { mongodbURI } = require('./config.js');
 console.log(mongodbURI);
 // needs a url in format...
@@ -29,6 +29,46 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  
+    // // Provide the name of the database and collection you want to use.
+    // // If the database and/or collection do not exist, the driver and Atlas
+    // // will create them automatically when you first write data.
+    // const dbName = "bookshelf-db";
+    // const collectionName = "books";
+
+    // // Create references to the database and collection in order to run
+    // // operations on them.
+    // const database = client.db(dbName);
+    // const collection = database.collection(collectionName);
+
+    // // *** INSERT DOCUMENTS ***
+    // const books = 
+    // [
+    //   {
+    //     isbn: "978-0-141-19967-2",
+    //     title: "Sense and Sensibility",
+    //     author: "Jane Austen",    
+    //   },
+    //   {
+    //     isbn: "978-0-141-19907-8",
+    //     title: "Pride and Prejudice",
+    //     author: "Jane Austen",    
+    //   },
+    //   {
+    //     isbn: "978-0-141-19952-8",
+    //     title: "Emma",
+    //     author: "Jane Austen",    
+    //   },
+    // ];
+
+    // try{
+    //   const insertManyResult = await collection.insertMany(books);
+    //   console.log(`${insertManyResult.insertedCount} documents successfully inserted.\n`);
+    // } catch (err) {
+    //     console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+    // }
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
